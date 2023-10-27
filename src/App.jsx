1ece17fa9a5efae09ac438758123a6e6ec06e6ae
unknown <aliopdandan@gmail.com>
2023-10-27 19:53:21 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,32 +5,54 @@ import { useEffect, useState } from "react";
 function App() {
   const [animeName, setAnimeName] = useState("");
   const [animeData, setAnimeData] = useState(null);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    
-    // Make the API request when the form is submitted
-    fetch(`https://kitsu.io/api/edge/anime?filter[text]=${animeName}`)
-      .then((res) => res.json())
-      .then((data) => {
-        setAnimeData(data); // Update the state with the retrieved data
-      })
-      .catch((error) => console.log(error));
+  
+  const fetchData = async () => {
+    const url = `https://myanimelist.p.rapidapi.com/anime/search/${animeName}`;
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': import.meta.env.VITE_RAPID_API_KEY,
+        'X-RapidAPI-Host': import.meta.env.VITE_X_RAPIDAPI_HOST,
+      },
+    };
+  
+    try {
+      // Check if you are within the rate limit before making a request
+      const currentTime = new Date().getTime();
+      if (!localStorage.getItem('lastRequestTime') || currentTime - localStorage.getItem('lastRequestTime') >= 3600000) {
+        const response = await fetch(url, options);
+        const result = await response.text();
+        console.log(result);
+  
+        // Update the last request time
+        localStorage.setItem('lastRequestTime', currentTime);
+      } else {
+        console.log('Rate limit exceeded. Wait for the next hour to make another request.');
+      }
+    } catch (error) {
+      console.error(error);
+    }
   };
-
+  
+  fetchData();
+  
   return (
     <div className="main">
       <h1>Anime Search App</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Search for anime..."
-          value={animeName}
-          onChange={(e) => setAnimeName(e.target.value)}
-        />
-        <button type="submit">Search</button>
+      <form>
+        <div className="data-inputs">
+          <input
+            type="text"
+            placeholder="Search for anime..."
+            value={animeName}
+            onChange={(e) => {
+              setAnimeName(e.target.value);
+            }}
+          />
+          <button type="submit">Search</button>
+        </div>
       </form>
-      <AnimeData/>
+      <AnimeData />
     </div>
   );
 }
