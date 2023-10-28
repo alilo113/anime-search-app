66f270941e6ae30d8ba25400892b09719fc03abf
unknown <aliopdandan@gmail.com>
2023-10-28 18:43:20 +0100

    commi

--- src/animeData.jsx
+++ src/animeData.jsx
@@ -9,23 +9,23 @@ export function AnimeData({ fetchedData }) {
   };
 
   return (
-    <div className="animeData">
-      <div>
-        {/* Display the fetched data */}
-        {fetchedData && fetchedData.attributes ? (
+    <div>
+      {fetchedData && fetchedData.attributes ? (
+        <div className='animeData'>
           <div>
             <img src={fetchedData.attributes.posterImage.original} alt="" />
+          </div>
+          <div>
             <h2>{fetchedData.attributes.titles.en}</h2>
             <p>{fetchedData.attributes.synopsis}</p>
             <p>Release Year: {fetchedData.attributes.startDate}</p>
             <p>Rating: {fetchedData.attributes.averageRating}</p>
-            {/* Ensure 'slug' property exists before accessing it */}
             {fetchedData.attributes.slug && <p>Name: {fetchedData.attributes.slug}</p>}
           </div>
-        ) : (
-          <p>No data available</p>
-        )}
-      </div>
+        </div>
+      ) : (
+        <p>Search for anime to be displayed...</p>
+      )}
     </div>
   );
 }
