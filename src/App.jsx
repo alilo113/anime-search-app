72b0a71ed71c1b9da5337f40d30f63074c7c39a3
unknown <aliopdandan@gmail.com>
2023-10-28 07:51:05 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,37 +5,27 @@ import { useEffect, useState } from "react";
 function App() {
   const [animeName, setAnimeName] = useState("");
   const [animeData, setAnimeData] = useState(null);
-  
-  const fetchData = async () => {
-    const url = `https://myanimelist.p.rapidapi.com/anime/search/${animeName}`;
+
+  const fetchData = async (name) => {
+    const url = 'https://myanimelist.p.rapidapi.com/anime/search/Death%20Note';
     const options = {
       method: 'GET',
       headers: {
-        'X-RapidAPI-Key': import.meta.env.VITE_RAPID_API_KEY,
-        'X-RapidAPI-Host': import.meta.env.VITE_X_RAPIDAPI_HOST,
-      },
-    };
-  
-    try {
-      // Check if you are within the rate limit before making a request
-      const currentTime = new Date().getTime();
-      if (!localStorage.getItem('lastRequestTime') || currentTime - localStorage.getItem('lastRequestTime') >= 3600000) {
+        'X-RapidAPI-Key': '59b28c036bmshd5cfad74e6da3e1p125809jsnf50e58147066',
+        'X-RapidAPI-Host': 'myanimelist.p.rapidapi.com'
+      }};
+      
+      try {
         const response = await fetch(url, options);
         const result = await response.text();
         console.log(result);
-  
-        // Update the last request time
-        localStorage.setItem('lastRequestTime', currentTime);
-      } else {
-        console.log('Rate limit exceeded. Wait for the next hour to make another request.');
+      } catch (error) {
+        console.error(error);
       }
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  
-  fetchData();
-  
+    };
+
+  fetchData()
+
   return (
     <div className="main">
       <h1>Anime Search App</h1>
